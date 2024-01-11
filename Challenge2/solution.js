b38f1d31376981ab30ff5184b86ab1dd6f668d94
XavierJEsteve <xjesteve@gmail.com>
2024-01-11 15:28:59 -0500

    Planned approach in Challenge2/solution.js

---
+++ Challenge2/solution.js
@@ -0,0 +1,21 @@
+// Soluition for something that is totally not fizzbuzz.
+
+/* Functionality
+*   - Accept a list of integers (any length)
+*   - Emit an error message if the list length is not a multiple of 10
+*   - Returns/prints a list of integers, with:
+        - items at positions which are a multiple of 2 or 3 removed.
+*/
+
+/* Assumptions
+*   - List positions start from zero
+*   - Any real/rational number is acceptable
+*/
+
+/* Safechecks
+*   - Make sure to check that it's actually a valid list of numbers
+*/
+
+/* Testing
+*   - Coming soon.
+*/
