c4dcde2bae3372423623895191ba4de54cbbe0a3
XavierJEsteve <xjesteve@gmail.com>
2024-01-12 09:30:55 -0500

    Filtering function looks good and seems to function with validation check in place to ensure numeric data and length%10==0. Goiing to make some tests in a separate file.

--- Challenge2/solution.js
+++ Challenge2/solution.js
@@ -12,10 +12,64 @@
 *   - Any real/rational number is acceptable
 */
 
-/* Safechecks
-*   - Make sure to check that it's actually a valid list of numbers
-*/
+allTests()
 
-/* Testing
-*   - Coming soon.
+// Skipping numbers in position multiples of ['x1, x2, xn']
+function filterList(given, badMults) {
+    // Safechecks
+        // Make sure to check that it's actually a valid list of numbers
+        const isValid = validateInput(given)
+
+        if (!isValid) {
+            console.error("ERROR::input:: Array<number> required with length a multiple of 10 required")
+            return 1;
+        }
+        else {            // Do the thing
+            console.debug(' INPUT: ', given, '\n')
+            // boring...
+            // for (let i = 0; i < given.length; i++ ) {
+            // }
+
+            // creates a shallow copy of given array, filtered for elements that pass the test in arrow fuinction
+            const output = given.filter((_, index) => { // omit callback (_)...one-liner returned instead
+                // Include position 0, and return elements NOT at bad multiples
+                return index === 0 || badMults.every(multiple => index % multiple !== 0);
+            });
+            console.log(' OUTPUT: ', output, '\n')
+            return output
+        }
+}
+
+function validateInput(input) {
+    return (
+        Array.isArray(input) && input.every(element => typeof element === 'number') && ( input.length % 10 === 0 )
+    );
+}
+
+/* Testing  
 */
+function allTests(test_optional) {
+    const pass = 0
+    const fail = 1
+
+    const test_sanity   = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const test_length   = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
+    const test_numeric     = ['l', 'z', 'e', 4, 5, 'G', 7, 8, 9, 'O']
+    const excluded      = [2, 3]
+
+    // pass == 0, fail == 1
+    console.assert(
+        !filterList(test_sanity, excluded) ,
+        "TEST 1: Sanity check"
+    )
+
+    // console.assert(
+    //     filterList(test_length, excluded),
+    //     "TEST 2: Non multiple of 10"
+    // )
+
+    // console.assert(
+    //     filterList(test_numeric, excluded),
+    //     "TEST 3: Array & Numeric data check"
+    // )
+}
